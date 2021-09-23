3ddda09be558b84ff47fb3c27d4239ac0a3fdae7
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-09-23 12:13:04 +0200

    Initial commit

---
+++ src/main/java/net/tutorialbykaupenjoe/livestreammod/LivestreamMod.java
@@ -0,0 +1,38 @@
+package net.tutorialbykaupenjoe.livestreammod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(LivestreamMod.MOD_ID)
+public class LivestreamMod
+{
+    public static final String MOD_ID = "livestreammod";
+
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public LivestreamMod() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        eventBus.addListener(this::setup);
+        eventBus.addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+}
