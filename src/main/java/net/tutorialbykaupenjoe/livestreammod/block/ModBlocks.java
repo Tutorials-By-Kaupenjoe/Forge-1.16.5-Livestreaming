2f76904db13b87c605682162d5c6bf43388ef162
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-10-03 15:43:44 +0200

    added custom tooltips to Blocks!

--- src/main/java/net/tutorialbykaupenjoe/livestreammod/block/ModBlocks.java
+++ src/main/java/net/tutorialbykaupenjoe/livestreammod/block/ModBlocks.java
@@ -2,8 +2,14 @@ package net.tutorialbykaupenjoe.livestreammod.block;
 
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,6 +18,8 @@ import net.tutorialbykaupenjoe.livestreammod.LivestreamMod;
 import net.tutorialbykaupenjoe.livestreammod.item.ModItemGroup;
 import net.tutorialbykaupenjoe.livestreammod.item.ModItems;
 
+import javax.annotation.Nullable;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class ModBlocks {
@@ -19,10 +27,27 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, LivestreamMod.MOD_ID);
 
     public static final RegistryObject<Block> KAUPEN_CORAL_DEAD = registerBlock("kaupencoral_dead",
-            () -> new DeadCoralPlantBlock(AbstractBlock.Properties.create(Material.CORAL)));
+            () -> new DeadCoralPlantBlock(AbstractBlock.Properties.create(Material.CORAL)), "tooltip.block.livestreammod.kaupencoral_dead");
 
     public static final RegistryObject<Block> KAUPEN_CORAL = registerBlock("kaupencoral",
             () -> new CoralPlantBlock(ModBlocks.KAUPEN_CORAL_DEAD.get(), AbstractBlock.Properties.create(Material.CORAL)));
+
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, String tooltipKey) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tooltipKey);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block, String tooltipKey) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().group(ModItemGroup.LIVESTREAM_GROUP)) {
+            @Override
+            public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+                tooltip.add(new TranslationTextComponent(tooltipKey));
+                super.addInformation(stack, worldIn, tooltip, flagIn);
+            }
+        });
+    }
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
